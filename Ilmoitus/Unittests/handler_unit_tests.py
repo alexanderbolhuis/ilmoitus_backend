6cc47b384256b67e9880534553e5d7c8f41c0349
slemmen <slemmen@student.avans.nl>
2014-03-11 23:44:56 +0100

    Added authorization initializer
    - Added generic method that can be called instead of the usual
    set_up_custom_path, that will set up the test server and initialize it
    with one user being logged in (READ THE DOCUMENTATION ON
    setup_server_with_user FUNCTION!!!)
    -Minor change to test dat acreator to allow creation of persons with
    unique email adresses.

--- Ilmoitus/Unittests/handler_unit_tests.py
+++ Ilmoitus/Unittests/handler_unit_tests.py
@@ -24,7 +24,7 @@ class EmployeeHandlerTest(BaseTestClass):
         self.set_up_custom_path([(path, main_application.AllEmployeesHandler)])
         number_of_persons = random.randint(1, 10)
         for i in range(0, number_of_persons):
-            PersonDataCreator.create_valid_person_data()
+            PersonDataCreator.create_valid_person_data(i)
 
         self.negative_test_stub_handler(path, "get", 404)
 
@@ -36,3 +36,47 @@ class EmployeeHandlerTest(BaseTestClass):
         self.set_up_custom_path([(path, main_application.AllEmployeesHandler)])
 
         self.negative_test_stub_handler(path, "get", 404)
+
+
+class BaseAuthorizationHandler(BaseTestClass):
+    def setup_server_with_user(self, handler_routes, user_is_logged_in, user_is_admin):
+        """
+            Helper method to set-up all data needed in these unit tests.
+
+            :param handler_routes: List of tuples that contain all url and handlers that will be set-up for this test.
+            :param user_is_logged_in: Boolean that indicates whether a user mock-up should be made or not.
+            :param user_is_admin: String that indicates whether or not the user is admin or not.
+                Only valid values are '0' (no admin) and '1' (is admin). Any other values will raise an Exception.
+
+            :returns: A dictionary with the following key-value pairs:
+                -"random_person" : A random person of which the contents can be checked later on
+                    -"path" : A url path that contains the id of the random person at the end (example: "/auth/531857")
+        """
+        path = '/auth/(.*)'
+        self.set_up_custom_path(handler_routes)
+        self.testbed.init_user_stub()
+
+        number_of_persons = random.randint(3, 10)
+        random_person_index = random.randint(0, number_of_persons - 1)
+        random_person = None
+
+        random_person_index2 = 0
+        while random_person_index2 == random_person_index:  # make sure we have a unique number
+            random_person_index2 = random.randint(0, number_of_persons - 1)
+        random_person2 = None
+        for i in range(0, number_of_persons):
+            person = PersonDataCreator.create_valid_person_data(i)
+            if i == random_person_index:
+                random_person = person
+                #Now, initialize the person as a user in google's users API if the boolean param says so
+                if user_is_logged_in:
+                    self.testbed.setup_env(
+                        overwrite=True,
+                        USER_EMAIL=random_person.email,
+                        USER_ID=str(random_person.key.integer_id()),
+                        USER_IS_ADMIN=user_is_admin)
+                path = "/auth/" + str(random_person.key.integer_id())
+            elif i == random_person_index2:
+                random_person2 = person
+
+        return {"random_person": random_person, "path": path, "random_person2": random_person2}
