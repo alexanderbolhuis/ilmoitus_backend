b9001989083648427ce079bc850bedf735ea14b0
JohanBeekers <johan.beekers@gmail.com>
2014-05-22 16:47:46 +0200

    ST17 supervisor declarations page

--- Ilmoitus/static/javascript/controllers.js
+++ Ilmoitus/static/javascript/controllers.js
@@ -57,6 +57,7 @@ ilmoitusApp.controller('templateController', function($scope, $state) {
 	request.done(function(data){
 		$scope.userName = data.first_name + " " + data.last_name;
 		$scope.userId = data.employee_number;
+		$scope.userClass = data.class_name;
 		$scope.$apply();
 	});
 
@@ -338,8 +339,51 @@ ilmoitusApp.controller('newDeclarationController', function($scope, $state) {
 });
 
 
-ilmoitusApp.controller('declarationsSubmittedController', function($scope) {
+ilmoitusApp.controller('declarationsSubmittedController', function($scope, $state) {
 	$scope.navBtnSelect("sendedDeclarationsBtn");
+
+	var request = $.ajax({
+		type: "GET",
+		headers: {"Authorization": sessionStorage.token},
+		url: baseurl + "/declarations/supervisor",
+		crossDomain: true,
+		error: function(jqXHR, textStatus, errorThrown){
+			console.error( "Request failed: \ntextStatus: " + textStatus + " \nerrorThrown: "+errorThrown );
+		}
+	});
+
+	request.done(function(data){
+		console.log(data);
+		$scope.declarationList = data;
+		for(var i = 0 ; i < $scope.declarationList.length ; i++){
+			//turn created_at dates to actual javascript dates for comparison and string convertion.
+			$scope.declarationList[i].created_at = new Date($scope.declarationList[i].created_at);
+		}
+
+		//sort the array on creation date
+		$scope.declarationList.sort(function(a, b) {
+		    a = a.created_at;
+		    b = b.created_at;
+		    return a>b ? -1 : a<b ? 1 : 0;
+		});
+		$scope.$apply();
+	});
+		
+	//Select declaration
+	$scope.selectDeclaration = function(declaration){
+		$scope.currentdeclaration = declaration;
+	}
+	
+	//Doubleclick declaration
+	$scope.openDeclarationDetails = function(declarationid){
+ 		$state.go('template.sentDeclarationDetails', {declarationId: declarationid});
+  	}
+	
+	//Open declaration button
+	$scope.openDeclarationDetailsBtn = function(declarationid){
+ 		$state.go('template.sentDeclarationDetails', {declarationId: $scope.currentdeclaration.id});
+  	}
+
 });
 
 ilmoitusApp.controller('sentDeclarationDetailsController', function($scope) {
