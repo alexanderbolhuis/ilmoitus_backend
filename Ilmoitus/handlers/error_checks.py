c5b737d906fecb4cedbbda8c06ce1b5e344ff294
RobinBlok <robinrobin_1@hotmail.com>
2014-06-04 13:57:27 +0200

    BEIMP05
    Change / Add declaration handler

--- Ilmoitus/handlers/error_checks.py
+++ Ilmoitus/handlers/error_checks.py
@@ -91,11 +91,14 @@ def is_declaration_state(handler, declaration, class_name):
                                           "Expected state: " + class_name + " Received state: " + declaration.class_name)
 
 
-def has_post(handler, post, readable_post, break_if_missing=False):
+def has_post(handler, post, readable_post, break_if_missing=False, stringify=True):
     if handler.request.body is not None:
         request_body = json.loads(handler.request.body)
         if post in request_body.keys() and request_body[post] is not None and request_body[post] != "":
-            return str(request_body[post])
+            if stringify:
+                return str(request_body[post])
+            else:
+                return request_body[post]
     if break_if_missing:
         give_error_response(handler, 400, "Geen " + readable_post + " ontvangen.", post + " is None")
 
@@ -122,3 +125,43 @@ def is_allowed_declaration_viewer(handler, declaration, current_user):
        current_user.class_name != 'human_resources':
 
         give_error_response(handler, 401, "U heeft niet de juiste rechten om deze declaratie te openen")
+
+
+def if_employee_is_supervisor(handler, current_person):
+    if current_person.class_name != "supervisor":
+        give_error_response(handler, 401, "Kan de declaratie niet doorsturen. Deze persoon is geen leidinggevende",
+                                          "current_person_object's id is not an supervisor")
+
+
+def find_declaration_sub_type(handler, sub_id):
+    safe_id = 0
+    try:
+        safe_id = long(sub_id)
+    except ValueError:
+        give_error_response(handler, 400, "Id is geen correcte waarde (" + str(sub_id) + ")")
+
+    sub_type = DeclarationSubType.get_by_id(safe_id)
+    if sub_type is None:
+        give_error_response(handler, 404, "sub type ["+str(safe_id)+"] niet gevonden", "sub_type is None")
+    return sub_type
+
+
+def is_complete_dict(handler, dictcoll, elements, list_name):
+    for key in elements:
+        if key not in dictcoll.keys() or dictcoll[key] is None or dictcoll[key] == "":
+            give_error_response(handler, 400, "Key "+key+" niet gevonden in " + list_name,
+                                              "Key "+key+" not found")
+
+
+def is_valid_declaration_attachment(handler, attachment):
+    data = attachment["file"].split(":")[0]
+    mime = attachment["file"].split(":")[1].split(";")[0]
+    base = attachment["file"].split(":")[1].split(";")[1].split(",")[0]
+
+    if data != "data" or base != "base64":
+        give_error_response(handler, 400, "Kan geen declaratie toevoegen. De opgestuurde bijlage gegevens kloppen niet.",
+                                          "The base64 string is incorrect.")
+
+    if mime != "application/pdf" and mime.split("/")[0] != "image":
+        give_error_response(handler, 400, "Kan geen declaratie toevoegen. Alleen pdf's of images zijn toegestaan.",
+                                          "MimeType does not equal application/pdf or image/*", more_info=mime)
