a5365a746504ac7f320b084b5d567dd22980d497
RobinBlok <robinrobin_1@hotmail.com>
2014-06-14 22:57:55 +0200

    ST19-1

--- Ilmoitus/handlers/employee_list.py
+++ Ilmoitus/handlers/employee_list.py
@@ -15,6 +15,43 @@ class SpecificEmployeeDetailsHandler(BaseRequestHandler):
             respond_with_object_details_by_id(self, Person, employee_id)
 
 
+class SupervisorEmployeeTotalHandler(BaseRequestHandler):
+    def get(self):
+        if self.is_logged_in():
+            # Does employee exist
+            supervisor = self.logged_in_person();
+            employee_list = []
+
+            person_query = Person.query(Person.supervisor == supervisor.key)
+            query_result = person_query.fetch(limit=self.get_header_limit(), offset=self.get_header_offset())
+
+            for employee in query_result:
+                # Find declarations for employee
+                accepted_declarations = Declaration.gql("WHERE created_by = :cb AND class_name = :cn", cb=employee.key, cn="human_resources_approved_declaration").fetch()
+                accepted = len(accepted_declarations)
+
+                open_declarations = Declaration.gql("WHERE created_by = :cb AND class_name = :cn", cb=employee.key, cn="open_declaration").fetch()
+                open = len(open_declarations)
+
+                sv_denied_declarations = Declaration.gql("WHERE created_by = :cb AND class_name = :cn", cb=employee.key, cn="supervisor_declined_declaration").fetch()
+                sv_denied = len(sv_denied_declarations)
+
+                hr_denied_declarations = Declaration.gql("WHERE created_by = :cb AND class_name = :cn", cb=employee.key, cn="human_resources_declined_declaration").fetch()
+                hr_denied = len(hr_denied_declarations)
+
+                total_cost = 0
+                for declaration in accepted_declarations:
+                    total_cost += declaration.items_total_price
+
+                employee_list.append({"employee": employee.get_object_as_data_dict(),
+                                      "open_declarations": open,
+                                      "accepted_declarations": accepted,
+                                      "denied_declarations": (hr_denied + sv_denied),
+                                      "total_declarated_price": total_cost})
+
+            give_response(self, json.dumps(employee_list))
+
+
 class SpecificEmployeeTotalDeclarationsHandler(BaseRequestHandler):
     def get(self, employee_id):
         if self.is_logged_in():
